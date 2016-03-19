3854ce2d92b72b02637badd8faed8b9bf391fa62
smicl11 <bryan.smith094@gmail.com>
2016-03-19 13:35:10 -0700

    Boxes and button setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
+
+  var X = "X";
+
+  var O = "O";
+
+  var playerMove = function playerMove(){
+    if (player == O)
+      player = X;
+      else
+    player = O;
+  };
+
+
+  $(".box").on("click", function mark(event) {
+    $(this).text(playerMove);
+  });
+
+
+  $(".btn").on("click", function refreshPage(event) {
+    location.reload();
+  });
+
   // all code to manipulate the DOM
   // goes inside this function
 
