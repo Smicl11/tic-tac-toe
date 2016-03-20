bcb6fcd4978709ab8656318885f2847ea4de5b0a
smicl11 <bryan.smith094@gmail.com>
2016-03-19 21:41:57 -0700

    Updated X win scenarios and commented code.

--- js/app.js
+++ js/app.js
@@ -4,22 +4,16 @@ var winningCombinations;
 
 // wait for the DOM to finish loading
 $(document).ready(function()  {
- // all code to manipulate the DOM
+// all code to manipulate the DOM
 
 var playerOne = "X";
     playerTwo = "O";
+    boxCells = $(".box");
     clickCount = 0;
-    winRowOne = [$("#bx0").text() + $("#bx1").text() + $("#bx2").text()];
-    winRowTwo = $("#bx0", "#bx1", "#bx3").text();
-    winRowThree = $("#bx0", "#bx1", "#bx3").text();
-    winColOne = $("#bx0", "#bx1", "#bx3").text();
-    winColTwo = $("#bx0", "#bx1", "#bx3").text();
-    winColThree = $("#bx0", "#bx1", "#bx3").text();
-    winDiagOne = $("#bx0", "#bx1", "#bx3").text();
-    winDiagTwo = $("#bx0", "#bx1", "#bx3").text();
 
-console.log(winRowOne);
 
+//Cycle between X's and O's and prevent overwriting what's already in the box.
+//The NOT (!) symbol prevents overwriting and already taken box.
  $(".box").click(function(event){
    if(! $(this).text() ){
      if (clickCount % 2 === 0 ){
@@ -33,23 +27,54 @@ console.log(winRowOne);
    }
  });
 
+//Not sure if I need this right now.
+/*
+function xWins() {
+  alert("X Wins!");
+}
+*/
+
+function winner() {
+ if (($(boxCells.eq(0)).text() === 'X' &&
+      $(boxCells.eq(1)).text() === 'X' &&
+      $(boxCells.eq(2)).text() === 'X') ||
+
+    ($(boxCells.eq(3)).text() === 'X' &&
+      $(boxCells.eq(4)).text() === 'X' &&
+      $(boxCells.eq(5)).text() === 'X') ||
+
+    ($(boxCells.eq(6)).text() === 'X' &&
+      $(boxCells.eq(7)).text() === 'X' &&
+      $(boxCells.eq(8)).text() === 'X') ||
+
+    ($(boxCells.eq(0)).text() === 'X' &&
+      $(boxCells.eq(3)).text() === 'X' &&
+      $(boxCells.eq(6)).text() === 'X') ||
+
+    ($(boxCells.eq(1)).text() === 'X' &&
+      $(boxCells.eq(4)).text() === 'X' &&
+      $(boxCells.eq(7)).text() === 'X') ||
+
+    ($(boxCells.eq(2)).text() === 'X' &&
+      $(boxCells.eq(5)).text() === 'X' &&
+      $(boxCells.eq(8)).text() === 'X') ||
+
+    ($(boxCells.eq(0)).text() === 'X' &&
+      $(boxCells.eq(4)).text() === 'X' &&
+      $(boxCells.eq(8)).text() === 'X')) {
+        //return alert("X Wins!");
+        //return some value that is returned when winner() is true
+      }
+}
+
+//for loop to call the winner function?
+winner();
+
   $(".btn").on("click", function resetGame(event) {
     alert("Game reset!");
     window.location.reload();
   });
 
-if (winRowOne === "XXX" || "OOO",
-    winRowTwo === "X" || "O",
-    winRowThree === "X" || "O",
-    winColOne === "X" || "O",
-    winColTwo === "X" || "O",
-    winColThree === "X" || "O",
-    winDiagOne === "X" || "O",
-    winDiagTwo === "X" || "O") {
-      alert("You Win");
-    }
-    else {
-      alert("Draw");
-    }
+
 
 });
