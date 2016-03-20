d81e390c4b5644f0c01ff83c9f07c183c5f5f112
smicl11 <bryan.smith094@gmail.com>
2016-03-19 18:11:26 -0700

    ugh

--- js/app.js
+++ js/app.js
@@ -1,28 +1,55 @@
+//Global Variables
+var winningCombinations;
+
+
 // wait for the DOM to finish loading
-$(document).ready(function() {
+$(document).ready(function()  {
+ // all code to manipulate the DOM
 
-  var X = "X";
+var playerOne = "X";
+    playerTwo = "O";
+    clickCount = 0;
+    winRowOne = $("#bx0", "#bx1", "#bx3").text();
+    winRowTwo = $("#bx0", "#bx1", "#bx3").text();
+    winRowThree = $("#bx0", "#bx1", "#bx3").text();
+    winColOne = $("#bx0", "#bx1", "#bx3").text();
+    winColTwo = $("#bx0", "#bx1", "#bx3").text();
+    winColThree = $("#bx0", "#bx1", "#bx3").text();
+    winDiagOne = $("#bx0", "#bx1", "#bx3").text();
+    winDiagTwo = $("#bx0", "#bx1", "#bx3").text();
 
-  var O = "O";
+console.log(winRowOne);
 
-  var playerMove = function playerMove(){
-    if (player == O)
-      player = X;
-      else
-    player = O;
-  };
+ $(".box").click(function(event){
+   if( !$(this).text() ){
+     if (clickCount % 2 === 0 ){
+       $(this).text(playerOne);
+         clickCount ++;
+     }else if(clickCount % 2 !== 0){
+         $(this).text(playerTwo);
+         clickCount ++;
 
+     }
+   }
+ });
 
-  $(".box").on("click", function mark(event) {
-    $(this).text(playerMove);
+  $(".btn").on("click", function resetGame(event) {
+    alert("Game reset!");
+    $(".box").text("");
   });
 
-
-  $(".btn").on("click", function refreshPage(event) {
-    location.reload();
-  });
-
-  // all code to manipulate the DOM
-  // goes inside this function
+if (winRowOne === "X" || "O",
+    winRowTwo === "X" || "O",
+    winRowThree === "X" || "O",
+    winColOne === "X" || "O",
+    winColTwo === "X" || "O",
+    winColThree === "X" || "O",
+    winDiagOne === "X" || "O",
+    winDiagTwo === "X" || "O") {
+      alert("You Win");
+    }
+    else {
+      alert("Draw");
+    }
 
 });
